76151bfe3424b4677584474c7283184a77fb4498
dira205 <indirameidiyanti2@gmail.com>
2025-06-01 10:00:11 +0700

    Update data_saya.dart

--- lib/data_saya.dart
+++ lib/data_saya.dart
@@ -1,4 +1,4 @@
 class DataSaya {
    static String nama = "Indira";
-    static String gambar = "assets/mahasiswa/Indira Meidiyanti.jpeg";
+    static String gambar = "assets/gambar_dosen/Indira Meidiyanti.jpeg";
 }
